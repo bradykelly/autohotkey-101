5f8614ec57f738042e6b908e1065db2218be846c
= <=>
2023-12-26 13:35:41 +0200

    Work in progress to query config.json

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# autohotkey-aho101
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
+
+## autohotkey-aho101
 
 My start in AutoHotKey. This repo will contain many little experiments and lessons with ahk.
 
-## Bookmark link to bookmark folder
+### Experiments
 
-Right click a link on the current page and drag it to a certain bookmarks folder.
+#### Search whole `{$projects}` folder for TODO comments marked as global
+
+A CLI command that will get all specially tagged TODO items in all projects in your projects folder.
+
+#### Bookmark link to bookmark folder
+
+Right-click a link on the current page and drag it to a certain bookmarks folder. 
+Just like right-clicking a link and choosing the "Bookmark link command but you can choose which folder"
+
+
---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import json
+from jsam import CONFIG_FILE_PATH
+from common.constants import CONFIG_FILE_PATH
+
+
+def get_all_projects_todos(projects_folder: str):
+    """
+    Get all projects and their todos from the projects folder
+    :param projects_folder: the path to the projects folder
+    :return: a list of projects and their todos
+    """
+
+    projects = os.listdir(projects_folder)
+    projects_todos = []
+    for project in projects:
+        project_todos = []
+        project_path = os.path.join(projects_folder, project)
+        if os.path.isdir(project_path):
+            todos = os.listdir(project_path)
+            for todo in todos:
+                todo_path = os.path.join(project_path, todo)
+                if os.path.isfile(todo_path):
+                    project_todos.append(todo)
+        projects_todos.append((project, project_todos))
+    return projects_todos
+
+
+def read_file_extensions(language: str):
+    """
+    Read the file extensions from the "source_extensions" section of a
+     JSON config file global to the project with many sections.
+
+     :throws: FileNotFoundError if the JSON file does not exist
+    :return: a list of file extensions.
+    """
+
+    with open(CONFIG_FILE_PATH, "r") as config_file:
+        config = json.load(config_file)
+        return config["source_extensions"]
